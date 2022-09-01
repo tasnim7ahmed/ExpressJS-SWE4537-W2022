201c224a9fc9f9db10f4c0ec052f95d464a0f1f9
Tasnim Ahmed <tasnimahmed@iut-dhaka.edu>
2022-09-02 00:46:36 +0600

    Basic Controllers

--- Basics/router.js
+++ Basics/router.js
@@ -1,27 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const { getCV } = require("./controllers/CvController");
+
 const fs = require("fs");
 
-router.get("/", (req, res) => {
-  res.send("Home Page");
-});
-
-router.get("/about", (req, res) => {
-  res.send("About Page");
-});
-
-router.get("/cv", (req, res) => {
-  educations = fs.readFileSync("data/education", { encoding: "utf-8" });
-  educations = JSON.parse(String(educations));
-
-  edus = [];
-
-  for (let key in educations) {
-    edus.push(educations[key]);
-  }
-
-  res.render("cv", { name: "Tasnim Ahmed", educations: edus });
-});
+router.get("/", getCV);
 
 module.exports = router;
