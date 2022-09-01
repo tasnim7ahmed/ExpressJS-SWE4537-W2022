51252f596f36a7d650a30eda0a19af9826b3d555
Tasnim Ahmed <tasnimahmed@iut-dhaka.edu>
2022-09-02 00:40:42 +0600

    EJS with JSON and Texts

--- Basics/router.js
+++ Basics/router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const fs = require("fs");
+
 router.get("/", (req, res) => {
   res.send("Home Page");
 });
@@ -10,7 +12,16 @@ router.get("/about", (req, res) => {
 });
 
 router.get("/cv", (req, res) => {
-  res.render("cv", { name: "Tasnim Ahmed" });
+  educations = fs.readFileSync("data/education", { encoding: "utf-8" });
+  educations = JSON.parse(String(educations));
+
+  edus = [];
+
+  for (let key in educations) {
+    edus.push(educations[key]);
+  }
+
+  res.render("cv", { name: "Tasnim Ahmed", educations: edus });
 });
 
 module.exports = router;
